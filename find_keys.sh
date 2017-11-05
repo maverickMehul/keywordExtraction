4328f104f3559cbf6280832b2781b8153f9f8896
Mehul Dhikonia` <mehul.dhikonia@gmail.com>
2017-11-05 13:20:53 +0530

    delete temp file

--- find_keys.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup /usr/bin/python /home/Desktop/maroon-ai^maroon-ai/Keyword_Extraction/RAKE/find_keys_linkedin.py > /home/Desktop/maroon-ai^maroon-ai/Keyword_Extraction/RAKE/Keys.out 2>&1&
